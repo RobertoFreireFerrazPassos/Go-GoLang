17b1e6d40d7f181b951b7ae14b290031e31d1250
Roberto Freire <robertofreireferrazpassos@gmail.com>
2022-04-24 22:26:29 -0300

    add variable and functions in Go lang

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Go-GoLang-Design-Patterns
+
+All the basics about the Go language
+
+Design Patterns in Go
---
+++ language/main.go
@@ -0,0 +1,49 @@
+package main
+
+// go build main.go
+// go run main.go
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+) // import others packages
+
+func main() {
+	sliceExample()
+}
+
+// base go types: string, integer, float, array, map
+
+func sliceExample() {
+	// array that can grow and shrink
+	texts := []string{"A1", "C3"}
+	texts = append(texts, "F3")
+	fmt.Println(texts)
+
+	for i, text := range texts {
+		fmt.Println(i, text)
+	}
+}
+
+func funcExample() {
+	var variable = innerFuncExample("test")
+	fmt.Println(variable)
+}
+
+func innerFuncExample(text string) string {
+	return "Variable: " + text
+}
+
+func variableDeclaration() {
+	card := "Variable 1" // := declaring a new variable and implicit understanding the type
+	card = "Variable 2"
+	fmt.Println(card)
+}
+
+func printFunc() {
+	fmt.Println("Hi There!")
+	input := bufio.NewScanner(os.Stdin)
+	input.Scan()
+	fmt.Println(input.Text())
+}
